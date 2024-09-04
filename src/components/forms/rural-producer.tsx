e746415ca4e69802c520c1b94e7cf473617d919d
gabriel-silveira <gabriel@gabrielsilveira.com.br>
2024-09-04 17:41:05 -0300

    Rural producer form (WIP)

--- src/components/forms/rural-producer.tsx
+++ src/components/forms/rural-producer.tsx
@@ -1,43 +1,14 @@
 import React from 'react';
-import {useState} from 'react';
-import {Label, Listbox, ListboxButton, ListboxOption, ListboxOptions} from '@headlessui/react';
-import {CheckIcon, ChevronUpDownIcon} from '@heroicons/react/20/solid';
-
-const people = [
-  {name: "Acre", acronym: "AC"},
-  {name: "Alagoas", acronym: "AL"},
-  {name: "Amapá", acronym: "AP"},
-  {name: "Amazonas", acronym: "AM"},
-  {name: "Bahia", acronym: "BA"},
-  {name: "Ceará", acronym: "CE"},
-  {name: "Distrito Federal", acronym: "DF"},
-  {name: "Espírito Santo", acronym: "ES"},
-  {name: "Goiás", acronym: "GO"},
-  {name: "Maranhão", acronym: "MA"},
-  {name: "Mato Grosso", acronym: "MT"},
-  {name: "Mato Grosso do Sul", acronym: "MS"},
-  {name: "Minas Gerais", acronym: "MG"},
-  {name: "Pará", acronym: "PA"},
-  {name: "Paraíba", acronym: "PB"},
-  {name: "Paraná", acronym: "PR"},
-  {name: "Pernambuco", acronym: "PE"},
-  {name: "Piauí", acronym: "PI"},
-  {name: "Rio de Janeiro", acronym: "RJ"},
-  {name: "Rio Grande do Norte", acronym: "RN"},
-  {name: "Rio Grande do Sul", acronym: "RS"},
-  {name: "Rondônia", acronym: "RO"},
-  {name: "Roraima", acronym: "RR"},
-  {name: "Santa Catarina", acronym: "SC"},
-  {name: "São Paulo", acronym: "SP"},
-  {name: "Sergipe", acronym: "SE"},
-  {name: "Tocantins", acronym: "TO"},
-];
+import {useNavigate} from "react-router-dom";
+import BrazilStatesListBox from "../listboxes/brazil-states";
 
 function RuralProducerForm() {
-  const [selected, setSelected] = useState(people[0]);
+  const navigate = useNavigate();
+
+  const goHome = () => navigate('/');
 
   return (
-    <div>
+    <div className="card">
       <div className="flex-container rural-producer-form-first-row">
         <div className="flex-item">
           <label className="block text-sm font-medium leading-6 text-gray-900">
@@ -92,59 +63,113 @@ function RuralProducerForm() {
         </div>
 
         <div className="flex-item">
-          <Listbox value={selected} onChange={setSelected}>
-            <Label className="block text-sm font-medium leading-6 text-gray-900">Estado</Label>
-
-            <div className="relative mt-2">
-              <ListboxButton
-                className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10
-                text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none
-                focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6"
-              >
-                <span className="flex items-center">
-                  <span className="ml-1 block truncate">{selected.name}</span>
-                </span>
-                <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
-                  <ChevronUpDownIcon aria-hidden="true" className="h-5 w-5 text-gray-400"/>
-                </span>
-              </ListboxButton>
-
-              <ListboxOptions
-                transition
-                className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none data-[closed]:data-[leave]:opacity-0 data-[leave]:transition data-[leave]:duration-100 data-[leave]:ease-in sm:text-sm"
-              >
-                {people.map((person) => (
-                  <ListboxOption
-                    key={person.acronym}
-                    value={person}
-                    className="group relative cursor-default select-none py-2 pl-3 pr-9 text-gray-900 data-[focus]:bg-indigo-600 data-[focus]:text-white"
-                  >
-                    <div className="flex items-center">
-                      <span className="ml-1 block truncate font-normal group-data-[selected]:font-semibold">
-                        {person.name}
-                      </span>
-                    </div>
-
-                    <span
-                      className="absolute inset-y-0 right-0 flex items-center pr-4
-                      text-indigo-600 group-data-[focus]:text-white [.group:not([data-selected])_&]:hidden"
-                    >
-                      <CheckIcon aria-hidden="true" className="h-5 w-5"/>
-                    </span>
-                  </ListboxOption>
-                ))}
-              </ListboxOptions>
-            </div>
-          </Listbox>
+          <BrazilStatesListBox/>
         </div>
       </div>
 
-      <div className="mt-4 w-full">
-        <button
-          className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-        >
-          Cadastrar
-        </button>
+      <div className="flex-container mt-5">
+        <div className="flex-item w-2 pr-2">
+          <label className="block text-sm font-medium leading-6 text-gray-900">
+            Área total em hectares da fazenda
+          </label>
+          <div className="mt-2">
+            <input
+              type="text"
+              required
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-slate-200 sm:text-sm sm:leading-6"
+            />
+          </div>
+        </div>
+
+        <div className="flex-item w-2 pl-2 pr-2">
+          <label className="block text-sm font-medium leading-6 text-gray-900">
+            Área agricultável em hectares
+          </label>
+          <div className="mt-2">
+            <input
+              type="text"
+              required
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-slate-200 sm:text-sm sm:leading-6"
+            />
+          </div>
+        </div>
+
+        <div className="flex-item w-2 pl-2 pr-2">
+          <label className="block text-sm font-medium leading-6 text-gray-900">
+            Área de vegetação em hectares
+          </label>
+          <div className="mt-2">
+            <input
+              type="text"
+              required
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-slate-200 sm:text-sm sm:leading-6"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="flex-container mt-5">
+        <div className="flex-item">
+          <label className="block text-sm font-medium leading-6 text-gray-900">
+            Culturas plantadas
+          </label>
+
+          <div className="mt-2 ml-2">
+            <div className="cursor-pointer float-left mr-30">
+              <input type="checkbox" id="soja" name="soja" value="Soja"/>
+              <label htmlFor="soja">Soja</label>
+            </div>
+
+            <div className="cursor-pointer float-left mr-30">
+              <input type="checkbox" id="milho" name="milho" value="Milho"/>
+              <label htmlFor="milho">Milho</label>
+            </div>
+
+            <div className="cursor-pointer float-left mr-30">
+              <input type="checkbox" id="algodao" name="algodao" value="Algodão"/>
+              <label htmlFor="algodao">Algodão</label>
+            </div>
+
+            <div className="cursor-pointer float-left mr-30">
+              <input type="checkbox" id="cafe" name="cafe" value="Café"/>
+              <label htmlFor="cafe">Café</label>
+            </div>
+
+            <div className="cursor-pointer float-left">
+              <input type="checkbox" id="cana" name="cana" value="Cana de Açúcar"/>
+              <label htmlFor="cana">Cana de Açúcar</label>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <hr className="mt-5"/>
+
+      <div className="flex-container mt-5">
+        <div className="flex-item w-2"></div>
+        <div className="flex-item w-2"></div>
+        <div className="flex-item w-3"></div>
+        <div className="flex-item w-3"></div>
+
+        <div className="flex-item w-1 pr-2">
+          <button
+            className="bg-white hover:bg-gray-100 text-gray-800 font-semibold px-3 py-1.5
+            border border-gray-400 rounded shadow"
+            onClick={goHome}
+          >
+            Cancelar
+          </button>
+        </div>
+
+        <div className="flex-item w-1 pl-2">
+          <button
+            className="flex w-full justify-center rounded-md bg-indigo-600 font-semibold px-3 py-1.5
+            leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2
+            focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Cadastrar
+          </button>
+        </div>
       </div>
     </div>
   )
--- src/components/tables/rural-producers/index.tsx
+++ src/components/tables/rural-producers/index.tsx
@@ -11,8 +11,9 @@ function RuralProducersTable() {
   return (
     <div>
       <button
-        className="bg-white hover:bg-gray-100 text-gray-800 font-semibold
-          py-2 px-4 border border-gray-400 rounded shadow create-button"
+        className="flex justify-center rounded-md bg-indigo-600 font-semibold px-3 py-1.5
+            leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2
+            focus-visible:outline-offset-2 focus-visible:outline-indigo-600 create-button"
         onClick={goCreatePage}
       >
         Cadastrar
@@ -43,9 +44,9 @@ function RuralProducersTable() {
   )
 }
 
-function RuralProducersTableRow(ruralProducer: IRuralProducer) {
+function RuralProducersTableRow(ruralProducer: IRuralProducer, index: number) {
   return (
-    <tr className="hover:bg-slate-100 cursor-pointer odd:bg-white even:bg-slate-50">
+    <tr key={index} className="hover:bg-slate-100 cursor-pointer odd:bg-white even:bg-slate-50">
       <td className="px-4 py-2">{ruralProducer.document}</td>
       <td className="px-4 py-2">{ruralProducer.producer_name}</td>
       <td className="px-4 py-2">{ruralProducer.farm_name}</td>
