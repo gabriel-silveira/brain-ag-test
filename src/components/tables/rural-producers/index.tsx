a964e483f144a01aa5e97a7bc63ed2e983564014
gabriel-silveira <gabriel@gabrielsilveira.com.br>
2024-09-06 11:06:43 -0300

    Remove dialog (WIP)

--- src/components/tables/rural-producers/index.tsx
+++ src/components/tables/rural-producers/index.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import {IRuralProducer} from "../../../_interfaces/rural_producer";
 import {useNavigate} from "react-router-dom";
-import {useDispatch, useSelector} from "react-redux";
-import { PencilIcon } from '@heroicons/react/24/solid';
-import {setRuralProducer} from "../../../store/rural-producer/actions";
+import {useSelector} from "react-redux";
+import RuralProducerTableRow from "./row";
 
 function RuralProducersTable() {
   const navigate = useNavigate();
-  const dispatch = useDispatch();
 
   const goCreatePage = () => navigate('/create');
 
@@ -35,57 +32,19 @@ function RuralProducersTable() {
           <th className="px-4 py-2 text-start">Nome da fazenda</th>
           <th className="px-4 py-2 text-start">Cidade</th>
           <th className="px-4 py-2 text-start">Estado</th>
-          <th className="px-4 py-2 text-start">Área da fazenda</th>
-          <th className="px-4 py-2 text-start">Área agric.</th>
-          <th className="px-4 py-2 text-start">Área de veg.</th>
+          <th className="px-4 py-2">Área da fazenda</th>
+          <th className="px-4 py-2">Área agric.</th>
+          <th className="px-4 py-2">Área de veg.</th>
           <th className="px-4 py-2 text-start">Culturas plant.</th>
-          <th className="px-4 py-2 text-start">Ações</th>
+          <th className="px-4 py-2 text-start pl-5">Ações</th>
         </tr>
         </thead>
         <tbody className="divide-y divide-slate-200 bg-white text-slate-800">
-        {ruralProducers.map(RuralProducersTableRow)}
+        {ruralProducers.map(RuralProducerTableRow)}
         </tbody>
       </table>
     </div>
   );
-
-  function RuralProducersTableRow(ruralProducer: IRuralProducer, index: number) {
-    const startEditing = () => {
-      dispatch(setRuralProducer({ ...ruralProducers[index] }));
-
-      navigate(`/edit/${index + 1}`);
-    };
-
-    return (
-      <tr key={index} className="hover:bg-slate-100 cursor-pointer odd:bg-white even:bg-slate-50">
-        <td className="px-4 py-2">{ruralProducer.document}</td>
-        <td className="px-4 py-2">{ruralProducer.producer_name}</td>
-        <td className="px-4 py-2">{ruralProducer.farm_name}</td>
-        <td className="px-4 py-2">{ruralProducer.city}</td>
-        <td className="px-4 py-2">{ruralProducer.state}</td>
-        <td className="px-4 py-2">{ruralProducer.farm_area}</td>
-        <td className="px-4 py-2">{ruralProducer.arable_area}</td>
-        <td className="px-4 py-2">{ruralProducer.vegetation_area}</td>
-        <td className="px-4 py-2">
-          {ruralProducer.crops_planted.map(CropPlanted)}
-        </td>
-        <td className="px-4 py-2">
-          <PencilIcon className="size-5 icon-button" onClick={startEditing} />
-        </td>
-      </tr>
-    )
-  }
-
-  function CropPlanted(value: string, index: number) {
-    return (
-      <span
-        className="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10 mr-1"
-        key={index}
-      >
-      {value}
-    </span>
-    )
-  }
 }
 
 export default RuralProducersTable;
---
+++ src/components/tables/rural-producers/row.tsx
@@ -0,0 +1,56 @@
+import React, {useState} from 'react';
+import {setRuralProducer} from "../../../store/rural-producer/actions";
+import {PencilIcon, TrashIcon} from "@heroicons/react/24/solid";
+import {useDispatch, useSelector} from "react-redux";
+import {IRuralProducer} from "../../../_interfaces/rural_producer";
+import {useNavigate} from "react-router-dom";
+import CropPlanted from "./crop-planted";
+import RemoveRuralProducerDialog from "../../dialogs/rural-producer/remove";
+
+function RuralProducerTableRow(ruralProducer: IRuralProducer, index: number) {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const [data, setData] = useState({
+    showRemoveDialog: false,
+  });
+
+  // @ts-ignore
+  const {ruralProducers} = useSelector(rootReducer => rootReducer.ruralProducerReducer);
+
+  const startEditing = () => {
+    dispatch(setRuralProducer({ ...ruralProducers[index] }));
+
+
+    navigate(`/edit/${index + 1}`);
+  };
+
+  const startRemoving = () => {
+    setData((prev) => ({showRemoveDialog: true}));
+  }
+
+  return (
+    <tr key={index} className="hover:bg-slate-100 cursor-pointer odd:bg-white even:bg-slate-50">
+      <td className="px-4 py-2">{ruralProducer.document}</td>
+      <td className="px-4 py-2">{ruralProducer.producer_name}</td>
+      <td className="px-4 py-2">{ruralProducer.farm_name}</td>
+      <td className="px-4 py-2">{ruralProducer.city}</td>
+      <td className="px-4 py-2">{ruralProducer.state}</td>
+      <td className="px-4 py-2 text-center">{ruralProducer.farm_area}</td>
+      <td className="px-4 py-2 text-center">{ruralProducer.arable_area}</td>
+      <td className="px-4 py-2 text-center">{ruralProducer.vegetation_area}</td>
+      <td className="px-4 py-2">
+        {ruralProducer.crops_planted.map(CropPlanted)}
+      </td>
+      <td className="px-4 py-2">
+        <PencilIcon className="size-5 icon-button float-left mr-2" onClick={startEditing} />
+
+        <TrashIcon className="size-5 icon-button" onClick={startRemoving} />
+
+        {data.showRemoveDialog ? (<RemoveRuralProducerDialog />) : ('')}
+      </td>
+    </tr>
+  )
+}
+
+export default RuralProducerTableRow;
