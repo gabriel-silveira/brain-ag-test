cbb5aefbdf136de014cb4c76ce63999b7f32bd25
gabriel-silveira <gabriel@gabrielsilveira.com.br>
2024-09-08 16:28:54 -0300

    inline-info

--- src/components/tables/rural-producers/index.tsx
+++ src/components/tables/rural-producers/index.tsx
@@ -29,8 +29,6 @@ function RuralProducersTable() {
         Cadastrar
       </button>
 
-      <RuralProducersDashboard/>
-
       <div>
         {loading ? (
           <div className="w-full flex justify-center pt-5 pb-5">
@@ -45,31 +43,35 @@ function RuralProducersTable() {
         ) : (
           <div>
             {ruralProducers.length ? (
-              <table
-                className="w-full divide-y divide-slate-200 round-grey-border table-small-font"
-              >
-                <thead className="bg-lighter-grey text-slate-800">
-                <tr>
-                  <th className="px-4 py-2 text-start">CPF / CNPJ</th>
-                  <th className="px-4 py-2 text-start">Nome do produtor</th>
-                  <th className="px-4 py-2 text-start">Nome da fazenda</th>
-                  <th className="px-4 py-2 text-start">Cidade</th>
-                  <th className="px-4 py-2 text-start">Estado</th>
-                  <th className="px-4 py-2">Área da fazenda</th>
-                  <th className="px-4 py-2">Área agric.</th>
-                  <th className="px-4 py-2">Área de veg.</th>
-                  <th className="px-4 py-2 text-start">Culturas plant.</th>
-                  <th className="px-4 py-2">Editar</th>
-                  <th className="px-4 py-2">Excluir</th>
-                </tr>
-                </thead>
+              <div>
+                <RuralProducersDashboard/>
 
-                <tbody className="divide-y divide-slate-200 bg-white text-slate-800">
-                {ruralProducers.map((ruralProducer, index) => RuralProducerTableRow(ruralProducer, index))}
-                </tbody>
-              </table>
+                <table
+                  className="w-full divide-y divide-slate-200 round-grey-border table-small-font"
+                >
+                  <thead className="bg-lighter-grey text-slate-800">
+                  <tr>
+                    <th className="px-4 py-2 text-start">CPF / CNPJ</th>
+                    <th className="px-4 py-2 text-start">Nome do produtor</th>
+                    <th className="px-4 py-2 text-start">Nome da fazenda</th>
+                    <th className="px-4 py-2 text-start">Cidade</th>
+                    <th className="px-4 py-2 text-start">Estado</th>
+                    <th className="px-4 py-2">Área da fazenda</th>
+                    <th className="px-4 py-2">Área agric.</th>
+                    <th className="px-4 py-2">Área de veg.</th>
+                    <th className="px-4 py-2 text-start">Culturas plant.</th>
+                    <th className="px-4 py-2">Editar</th>
+                    <th className="px-4 py-2">Excluir</th>
+                  </tr>
+                  </thead>
+
+                  <tbody className="divide-y divide-slate-200 bg-white text-slate-800">
+                  {ruralProducers.map((ruralProducer, index) => RuralProducerTableRow(ruralProducer, index))}
+                  </tbody>
+                </table>
+              </div>
             ) : (
-              <div>Nenhuma produtor rural cadastrado</div>
+              <div className="inline-info">Nenhuma produtor rural cadastrado</div>
             )}
           </div>
         )}
