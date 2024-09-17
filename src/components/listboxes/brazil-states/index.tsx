68303a932ddd65ba88dd63e9a5b5097472791a19
gabriel-silveira <gabriel@gabrielsilveira.com.br>
2024-09-17 15:28:45 -0300

    Hotfix

--- src/components/listboxes/brazil-states/index.tsx
+++ src/components/listboxes/brazil-states/index.tsx
@@ -138,7 +138,7 @@ function BrazilStatesListBox(props: BrazilStatesListBoxProps) {
 
   return (
     <Listbox value={selected} onChange={handleChange}>
-      <Label className="block text-sm font-medium leading-6 text-gray-900">Assigned to</Label>
+      <Label className="block text-sm font-medium leading-6 text-gray-900">Estado</Label>
       <div className="relative mt-2">
         <ListboxButton
           className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
